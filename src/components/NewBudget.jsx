af5fb9b717bf53fef7776d8891b24494c6c9d4ac
Luis Gonzalez <lmgl887@gmail.com>
2022-04-21 20:44:39 -0500

    validate budget form

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import Message from "./Message";
+
+const NewBudget = ({ budget, setBudget }) => {
+
+  const [message, setMessage] = useState('');
+
+  const handleBudget = (e) => {
+    e.preventDefault();
+
+    console.log('sending form');
+    if (!Number(budget) || Number(budget) < 0) {
+      setMessage('Invalid budget');
+    } else {
+      console.log('Valid budget');
+    }
+  }
 
 
-const NewBudget = ({budget, setBudget}) => {
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form className="formulario">
+      <form onSubmit={handleBudget} className="formulario">
         <div className="campo">
           <label>Define Budget</label>
           <input
-            className="nuevo-presupuesto" 
-            type="text" 
+            className="nuevo-presupuesto"
+            type="text"
             placeholder="Enter your budget"
             value={budget}
-            onChange={(e)=> setBudget(e.target.value)}/>
+            onChange={(e) => setBudget(e.target.value)} />
         </div>
         <input type="submit" value="add"></input>
+        {message && <Message type="error">{message}</Message>}
       </form>
     </div>
   )
