bd2711d46b22e63c7585110717d65b49b1142e1e
Luis Gonzalez <lmgl887@gmail.com>
2022-04-24 22:47:44 -0500

    add form fields

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,6 +25,35 @@ const Modal = ({setModal, animateModal, setAnimateModal}) => {
       </div>
       <form className={`formulario ${animateModal ? "animar" : "cerrar"}`}>
         <legend>New Expense</legend>
+        <div className="campo">
+          <label htmlFor="name">Expense name</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Enter expense name"/>
+        </div>
+        <div className="campo">
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            type="number"
+            placeholder="Enter amount: e.g. 300" />
+        </div>
+        <div className="campo">
+          <label htmlFor="category">Category</label>
+          <select id="category">
+            <option value="" disabled selected>-- Select --</option>
+            <option value="saving">Saving</option>
+            <option value="food">Food</option>
+            <option value="miscellaneous">Miscellaneous</option>
+            <option value="leisure">Leisure</option>
+            <option value="health">Health</option>
+            <option value="subscriptions">Subscriptions</option>
+          </select>
+        </div>
+        <input
+          type="submit"
+          value="Add Expense"/>
       </form>
     </div>
   )
