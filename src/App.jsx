15156f031e7dd090da70a1f2722a966606ecab46
Luis Gonzalez <lmgl887@gmail.com>
2022-04-24 21:27:48 -0500

    add button new expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconNewExpense from './img/new_expense.svg'
 
 function App() {
 
@@ -11,11 +12,19 @@ function App() {
   return (
     <div>
       <Header
-      budget = {budget}
-      setBudget = {setBudget}
-      isValidBudget = {isValidBudget}
-      setIsValidBudget = {setIsValidBudget}
+        budget={budget}
+        setBudget={setBudget}
+        isValidBudget={isValidBudget}
+        setIsValidBudget={setIsValidBudget}
       />
+      {isValidBudget && (
+        <div className='nuevo-gasto'>
+          <img
+            src={IconNewExpense}
+            alt="icon new expense" />
+        </div>
+      )}
+
     </div>
   )
 }
